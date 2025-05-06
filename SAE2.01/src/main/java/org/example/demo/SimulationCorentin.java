f29f2c7f7559c84d58b5f3cb11a57b6aa9d4fb51
correntin <depariscorentin@gmail.com>
2025-05-06 16:50:55 +0200

    test com

--- SAE2.01/src/main/java/org/example/demo/SimulationCorentin.java
+++ SAE2.01/src/main/java/org/example/demo/SimulationCorentin.java
@@ -3,13 +3,13 @@ package org.example.demo;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
@@ -28,12 +28,23 @@ public class SimulationCorentin extends Application {
 
         Button btn1 = new Button("jouer");
         Button btn2 = new Button("règles du jeu");
+        VBox vbox = new VBox(btn1);
+        VBox vbox2 = new VBox(btn2);
 
-        root.getChildren().addAll(tf,btn1,btn2);
+
 
         FXMLLoader fxmlLoader = new FXMLLoader(Simulation.class.getResource("simul.fxml"));
 
         Scene scene = new Scene(root, 700, 500);
+
+        vbox.setTranslateX(scene.getWidth()/2);
+        vbox.setTranslateY(scene.getHeight()-scene.getHeight()/2.5);
+
+        vbox2.setTranslateX(scene.getWidth()/2);
+        vbox2.setTranslateY(scene.getHeight()-scene.getHeight()/3);
+
+        root.getChildren().addAll(tf,vbox,vbox2);
+
 
         stage.setTitle("Mange si tu peux !");
         stage.setScene(scene);
